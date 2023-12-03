d5c5cf85972e1c89ac0450bed3117b2f31170c7a
MrBabuls <enoka008@edu.xamk.fi>
2023-12-03 16:54:06 +0200

    Game of life

--- Assets/Scripts/GameOfLife.cs
+++ Assets/Scripts/GameOfLife.cs
@@ -1,136 +1,169 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
 
 public class GameOfLife : MonoBehaviour
 {
-    private enum Seeds
+    private enum Seed
     {
+        FullTexture,
         RPentomino,
         Acorn,
-        GosperGun,
-        FullTexture
+        GosperGun
     }
-    
-    [SerializeField] private ComputeShader Simulator;
-    [SerializeField] private Material PlaneMaterial;
-    //[SerializeField] private GameInit Seed;
-    [SerializeField] private Seeds Seed;
-    [SerializeField] private Color CellCol;
-    [SerializeField][Range(0f,2f)] private float UpdateInterval;
-    
-    private float NextUpdate = 2f;
+
+    [SerializeField] private ComputeShader simulator;
+    [SerializeField] private Material planeMaterial;
+    [SerializeField] private Seed seed;
+    [SerializeField] private Color cellCol = Color.magenta;
+    [SerializeField] [Range(0f, 2f)] private float updateInterval;
+
+    private float _nextUpdate = 0;
+    private bool _isState1 = true;
 
     private static readonly Vector2Int TexSize = new Vector2Int(512, 512);
-    
-    private RenderTexture State1;
-    private RenderTexture State2;
 
-    private bool IsState1;
-    
-    private static int Update1Kernel;
-    private static int Update2Kernel;
-    
-    private static int FullTexKernel;
-    private static int RPentKernel;
-    private static int AcornKernel;
-    private static int GunKernel;
-    
+    private RenderTexture _state1;
+    private RenderTexture _state2;
+
+    private static int _update1Kernel;
+    private static int _update2Kernel;
+    private static int _seedKernel;
+
     private static readonly int BaseMap = Shader.PropertyToID("_BaseMap");
-    private static readonly int CellColour = Shader.PropertyToID("CellColour");
+    private static readonly int CellColor = Shader.PropertyToID("CellColor");
     private static readonly int TextureSize = Shader.PropertyToID("TextureSize");
     private static readonly int State1Tex = Shader.PropertyToID("State1");
     private static readonly int State2Tex = Shader.PropertyToID("State2");
+    private static readonly int WrapBool = Shader.PropertyToID("WrapBool");
 
-    void Start()
+    private void Awake()
     {
-        State1 = new RenderTexture(TexSize.x, TexSize.y, 0, DefaultFormat.LDR)
+        CreateRenderTextures();
+        FindKernels();
+        InitializeSimulation();
+    }
+
+    private void CreateRenderTextures()
+    {
+        _state1 = CreateRenderTexture();
+        _state2 = CreateRenderTexture();
+        planeMaterial.SetTexture(BaseMap, _state1);
+    }
+
+    private RenderTexture CreateRenderTexture()
+    {
+        RenderTexture texture = new RenderTexture(TexSize.x, TexSize.y, 0, DefaultFormat.LDR)
         {
             filterMode = FilterMode.Point,
             enableRandomWrite = true
         };
+        texture.Create();
+        return texture;
+    }
 
-        State1.Create();
+    private void FindKernels()
+    {
+        _update1Kernel = simulator.FindKernel("Update1");
+        _update2Kernel = simulator.FindKernel("Update2");
 
-        State2 = new RenderTexture(TexSize.x, TexSize.y, 0, DefaultFormat.LDR)
+        _seedKernel = seed switch
         {
-            filterMode = FilterMode.Point,
-            enableRandomWrite = true
+            Seed.FullTexture => simulator.FindKernel("InitFullTexture"),
+            Seed.RPentomino => simulator.FindKernel("InitRPentomino"),
+            Seed.Acorn => simulator.FindKernel("InitAcorn"),
+            Seed.GosperGun => simulator.FindKernel("InitGun"),
+            _ => 0
         };
 
-        State2.Create();
-        
-        Update1Kernel = Simulator.FindKernel("Update1");
-        Update2Kernel = Simulator.FindKernel("Update2");
-        RPentKernel = Simulator.FindKernel("InitRPentomino");
-        AcornKernel = Simulator.FindKernel("InitAcorn");
-        GunKernel = Simulator.FindKernel("InitGun");
-        FullTexKernel = Simulator.FindKernel("InitFullTexture");
-        
-        Simulator.SetTexture(Update1Kernel, State1Tex, State1);
-        Simulator.SetTexture(Update1Kernel, State2Tex, State2);
-        
-        Simulator.SetTexture(Update2Kernel, State1Tex, State1);
-        Simulator.SetTexture(Update2Kernel, State2Tex, State2);
-        
-        Simulator.SetTexture(RPentKernel, State1Tex, State1);
-        Simulator.SetTexture(AcornKernel, State1Tex, State1);
-        Simulator.SetTexture(GunKernel, State1Tex, State1);
-        Simulator.SetTexture(FullTexKernel, State1Tex, State1);
-        
-        Simulator.SetVector(CellColour, CellCol);
-        
-        //bonus:
-        Simulator.SetVector(TextureSize, new Vector4(TexSize.x, TexSize.y));
+        simulator.SetTexture(_update1Kernel, State1Tex, _state1);
+        simulator.SetTexture(_update1Kernel, State2Tex, _state2);
 
-        switch (Seed)
+        simulator.SetTexture(_update2Kernel, State1Tex, _state1);
+        simulator.SetTexture(_update2Kernel, State2Tex, _state2);
+
+        simulator.SetTexture(_seedKernel, State1Tex, _state1);
+        simulator.SetTexture(_seedKernel, State2Tex, _state2);
+
+        simulator.SetVector(CellColor, cellCol);
+
+        // Bonus:
+        simulator.SetVector(TextureSize, new Vector4(TexSize.x, TexSize.y));
+        simulator.Dispatch(_seedKernel, TexSize.x / 8, TexSize.y / 8, 1);
+    }
+
+    private void InitializeSimulation()
+    {
+        switch (seed)
         {
-            case Seeds.RPentomino:
-                Simulator.Dispatch(RPentKernel,TexSize.x / 8, TexSize.y / 8, 1);
+            case Seed.FullTexture:
+                InitializeFullTexture();
                 break;
-            case Seeds.Acorn:
-                Simulator.Dispatch(AcornKernel,TexSize.x / 8, TexSize.y / 8, 1);
+            case Seed.RPentomino:
+                InitializeRPentomino();
                 break;
-            case Seeds.GosperGun:
-                Simulator.Dispatch(GunKernel,TexSize.x / 8, TexSize.y / 8, 1);
+            case Seed.Acorn:
+                InitializeAcorn();
                 break;
-            case Seeds.FullTexture:
-                Simulator.Dispatch(FullTexKernel,TexSize.x / 8, TexSize.y / 8, 1);
+            case Seed.GosperGun:
+                InitializeGosperGun();
                 break;
             default:
                 break;
         }
+    }
 
-        Simulator.Dispatch(Seed switch
-        {
-            Seeds.RPentomino => Simulator.FindKernel("InitRPentomino"),
-            Seeds.Acorn => Simulator.FindKernel("InitAcorn"),
-            Seeds.GosperGun => Simulator.FindKernel("InitGun"),
-            Seeds.FullTexture => Simulator.FindKernel("InitFullTexture"),
-            _ => 0
-        }, TexSize.x / 8, TexSize.y / 8, 1);
+    private void InitializeFullTexture()
+    {
+        simulator.Dispatch(_seedKernel, TexSize.x / 8, TexSize.y / 8, 1);
+    }
+
+    private void InitializeRPentomino()
+    {
+        simulator.Dispatch(_seedKernel, TexSize.x / 8, TexSize.y / 8, 1);
+    }
+
+    private void InitializeAcorn()
+    {
+        simulator.Dispatch(_seedKernel, TexSize.x / 8, TexSize.y / 8, 1);
+    }
+
+    private void InitializeGosperGun()
+    {
+        simulator.Dispatch(_seedKernel, TexSize.x / 8, TexSize.y / 8, 1);
     }
 
     private void Update()
     {
-        if (Time.time < NextUpdate) return;
-        
-        IsState1 = !IsState1;
-        
-        PlaneMaterial.SetTexture(BaseMap, IsState1 ? State1 : State2);
+        _nextUpdate += Time.deltaTime;
+        if (_nextUpdate > updateInterval)
+        {
+            UpdateSimulation();
+            _nextUpdate = 0;
+        }
+    }
 
-        NextUpdate = Time.time + UpdateInterval;
+    private void UpdateSimulation()
+    {
+        planeMaterial.SetTexture(BaseMap, _isState1 ? _state1 : _state2);
+        int currentKernel = _isState1 ? _update1Kernel : _update2Kernel;
+        simulator.Dispatch(currentKernel, TexSize.x / 8, TexSize.y / 8, 1);
+
+        _isState1 = !_isState1;
     }
 
     private void OnDisable()
     {
-        State1.Release();
-        State2.Release();
+        ReleaseRenderTextures();
     }
 
     private void OnDestroy()
     {
-        State1.Release();
-        State2.Release();
+        ReleaseRenderTextures();
+    }
+
+    private void ReleaseRenderTextures()
+    {
+        _state1.Release();
+        _state2.Release();
     }
 }
